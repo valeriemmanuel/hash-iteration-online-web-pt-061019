de44a4aef1773e602708c4d0cd883898f885104f
Valerie Manuel <valerie.manuel11@gmail.com>
2019-07-02 23:25:04 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,19 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
+# suite_a:"Amanda Presley", 
+#suite_b: "Seymour Hoffman", 
 # suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_d:  "Charlie Chaplin", 
 # suite_e: "Crumpet the Elf"
-# }
+ }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  
+  winner= ""
+  passengers.each do |suite, name|
+  if suite ==:suite_a && name.starts_with?("A")
+    winner = name
+    puts winner
+  end
 end
+
+
